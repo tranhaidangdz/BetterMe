00b3c4ad0ca561bfd670a2e2ec8dccef435b514f
tranhaidangdz <141541516+tranhaidangdz@users.noreply.github.com>
2026-02-25 16:55:43 +0700

    Update dependencies: Navigation3 + Serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.example.betterme"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.betterme"
@@ -55,8 +52,16 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    // Navigation
-    implementation(libs.navigation.compose)
+
+    // Navigation 3
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+    implementation(libs.kotlinx.serialization.core)
+
+    // Coil 3
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 
     // Image loading
     implementation(libs.coil.compose)
