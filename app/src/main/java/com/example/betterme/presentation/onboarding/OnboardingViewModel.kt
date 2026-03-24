de73f0374373add167166abd9ad170d925885fd5
tranhaidangdz <141541516+tranhaidangdz@users.noreply.github.com>
2026-03-24 14:51:14 +0700

    feat(onBoarding): add onBoardingViewModel

---
+++ app/src/main/java/com/example/betterme/presentation/onboarding/OnboardingViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.betterme.presentation.onboarding
+
+import androidx.lifecycle.viewModelScope
+import com.example.betterme.base.BaseMviViewModel
+import com.example.betterme.data.local.datastore.DataStoreManager
+import kotlinx.coroutines.launch
+
+class OnboardingViewModel(
+    private val dataStoreManager: DataStoreManager
+) : BaseMviViewModel<OnboardingIntent, OnboardingState, OnboardingEvent>() {
+    override fun initState(): OnboardingState {
+        return OnboardingState()
+    }
+
+    override fun processIntent(intent: OnboardingIntent) {
+        when (intent) {
+            OnboardingIntent.NavigateToSignIn -> handleNavigateToSignIn()
+        }
+    }
+
+    private fun handleNavigateToSignIn() {
+        viewModelScope.launch {
+            dataStoreManager.setDoneFirstTime()
+            sendEvent(OnboardingEvent.NavigateToSignIn)
+        }
+    }
+}
