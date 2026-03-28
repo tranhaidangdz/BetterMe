29ac98ff6741233f92478246a0c70e960b842270
tranhaidangdz <141541516+tranhaidangdz@users.noreply.github.com>
2026-03-28 16:24:22 +0700

    feat( Repository): setup Repository: add HabitLogRepo(domain) and HabitLogRepoImpl(data)

---
+++ app/src/main/java/com/example/betterme/domain/repository/HabitLogRepository.kt
@@ -0,0 +1,19 @@
+package com.example.betterme.domain.repository
+
+import com.example.betterme.data.local.room.entities.HabitLogEntity
+import kotlinx.coroutines.flow.Flow
+
+interface HabitLogRepository {
+
+    fun getLogs(habitId: Int): Flow<List<HabitLogEntity>>
+
+    suspend fun getLogByDate(habitId: Int, date: Long): HabitLogEntity?
+
+    suspend fun addLog(log: HabitLogEntity)
+
+    suspend fun updateLog(log: HabitLogEntity)
+
+    suspend fun deleteLog(log: HabitLogEntity)
+
+    suspend fun countCompleted(habitId: Int): Int
+}
