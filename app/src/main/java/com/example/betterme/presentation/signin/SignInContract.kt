c3b2600118f2b09e0b3c82b576115e7ae9a0ae91
tranhaidangdz <141541516+tranhaidangdz@users.noreply.github.com>
2026-04-06 21:43:16 +0700

    feat( LottiAnimation ): LottieView and SignInContract for signInWith Google

---
+++ app/src/main/java/com/example/betterme/presentation/signin/SignInContract.kt
@@ -0,0 +1,19 @@
+package com.example.betterme.presentation.signin
+
+import android.app.Activity
+import com.example.betterme.base.MviIntent
+import com.example.betterme.base.MviSingleEvent
+import com.example.betterme.base.MviViewState
+
+data class SignInState(
+    val isLoading: Boolean = false
+) : MviViewState
+
+sealed class SignInIntent : MviIntent {
+    data class SignInWithGoogle(val activity: Activity) : SignInIntent()
+}
+
+sealed class SignInEvent : MviSingleEvent {
+    data object NavigateToHome : SignInEvent()
+    data object LoginError : SignInEvent()
+}
